7f6dbbe6a1f1428f5f7ca9e9ea8e0ba6d368092a
daulet <daubaimur@gmail.com>
2025-09-13 05:09:23 +0500

    add components  and pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import './style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import './index.css';
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
 
 createApp(App).mount('#app')
